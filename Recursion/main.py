1f7929639cb05212f00d0e34eac531ab49cd1859
tjhobbs1 <tyhobbs319@gmail.com>
2022-09-12 20:27:29 -0500

    Iterative vs Recursion Added

--- Recursion/main.py
+++ Recursion/main.py
@@ -10,9 +10,28 @@ def recursiveMethod(n):
         recursiveMethod(n-1)
         print(n)
 
+def powerOfTwo(n):
+    #Base Case
+    if n == 0:
+        return 1
+    else:
+        power = powerOfTwo(n-1)
+        x = power * 2
+        print(x)
+        return x
 
-# Press the green button in the gutter to run the script.
+def powerOfTwoIterative(n):
+    i = 0
+    power = 1
+    while i < n:
+        power = power * 2
+        i += 1
+    return power
+
+
 if __name__ == '__main__':
-   recursiveMethod(5)
+   #recursiveMethod(5)
+
+   print(powerOfTwo(5))
 
 
